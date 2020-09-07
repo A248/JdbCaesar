021a470478f46a8dc26cb8d24a375b57dd427a77
A248 <anandebeh@gmail.com>
2020-09-07 13:01:46 -0400

    Add integration test for transactions

---
+++ src/test/java/space/arim/jdbcaesar/it/test/TransactionIT.java
@@ -0,0 +1,75 @@
+/* 
+ * JdbCaesar
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * JdbCaesar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JdbCaesar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JdbCaesar. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
+ */
+package space.arim.jdbcaesar.it.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Savepoint;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import space.arim.jdbcaesar.JdbCaesar;
+import space.arim.jdbcaesar.it.JdbCaesarProvider;
+import space.arim.jdbcaesar.transact.TransactionQuerySource;
+
+public class TransactionIT {
+
+	@ParameterizedTest
+	@ArgumentsSource(JdbCaesarProvider.class)
+	public void testTransactions(JdbCaesar jdbCaesar) {
+		Object result = jdbCaesar.transaction().body((querySource, controller) -> {
+			querySource.query(
+					"CREATE TABLE transactions ("
+					+ "someNumber INT NOT NULL)")
+					.voidResult().execute();
+			Savepoint pointOfNothing = controller.savepoint("pointOfNothing");
+
+			querySource.query(
+					"INSERT INTO transactions (someNumber) VALUES (?)")
+					.params(1)
+					.voidResult().execute();
+			Savepoint pointOfOne = controller.savepoint("pointOfOne");
+
+			querySource.query("UPDATE transactions SET someNumber = ? WHERE someNumber = ?")
+					.params(3, 1)
+					.voidResult().execute();
+
+			assertSomeNumberValue(querySource, 3);
+
+			controller.rollbackTo(pointOfOne);
+
+			controller.release(pointOfNothing);
+
+			assertSomeNumberValue(querySource, 1);
+			return null;
+		}).onError(() -> null).execute();
+		assertNull(result);
+	}
+	
+	private void assertSomeNumberValue(TransactionQuerySource querySource, int expected) {
+		Integer actual = querySource.query(
+				"SELECT someNumber FROM transactions")
+				.singleResult((resultSet) -> resultSet.getInt("someNumber"))
+				.execute();
+		assertNotNull(actual);
+		assertEquals(expected, actual.intValue());
+	}
+	
+}
