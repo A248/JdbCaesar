77e289dd060f2d6ccc6d315fe0fa95914ce1dcfa
A248 <anandebeh@gmail.com>
2020-10-24 10:58:43 -0400

    Add module-info in Java 8-compatible manner

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module space.arim.jdbcaesar {
+	exports space.arim.jdbcaesar;
+	exports space.arim.jdbcaesar.adapter;
+	exports space.arim.jdbcaesar.error;
+	exports space.arim.jdbcaesar.mapper;
+	exports space.arim.jdbcaesar.query;
+	exports space.arim.jdbcaesar.transact;
+
+	requires transitive java.sql;
+}
